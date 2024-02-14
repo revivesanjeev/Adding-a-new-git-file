a8ad2c657966fcbb08a2945ce7462c1349ad19bb
revivesanjeev <revivesanjeev324@gmail.com>
2024-02-14 11:34:30 +0530

    3,4 program pushing to github

---
+++ 3.Understanding DOM & getElementById Method/index.js
@@ -0,0 +1,15 @@
+const mainheading=document.getElementById('main-heading');
+mainheading.textContent='Fruit World';
+mainheading.style.color='orange';
+
+
+const headercolor=document.getElementById('header');
+headercolor.style.backgroundColor='green';
+headercolor.style.borderBottom='2px solid orange';
+
+const basketheading=document.getElementById('basket-heading');
+basketheading.style.color='green' ;
+
+const last=document.getElementById('thanks');
+last.innerHTML='<p>Please visit us again</p>'
+
--- DOM-program2/index.js
+++
@@ -1,3 +0,0 @@
-
-
-console.log("This is sanjeev");
