eed3377ce6ddd25cd998f2a77967723b943622ec
revivesanjeev <revivesanjeev324@gmail.com>
2024-02-16 18:08:54 +0530

    10,11,12,13,14added and modified

--- 11.filterFunctionality/index.js
+++ 11.filterFunctionality/index.js
@@ -1,11 +1,40 @@
 
 
-// // Create the input element
+
+
+
+
+// //adding the new fruit to the list
+// const form = document.querySelector('form') ;
+// //selecting the class of fruits of UL using queryselector, bcz we will append created element in that form parallel to existing element
+// const fruits = document.querySelector('.fruits');
+
+// form.addEventListener('submit',function(event){
+// event.preventDefault();
+//  //select the input element
+// const fruitToAdd = document.getElementById('fruit-to-add');
+// // Created the Li for fruit
+//         const newLi = document.createElement('li');
+//         // const newLiText=document.createTextNode(fruitToAdd.value);
+//         // newLi.appendChild(newLiText);
+//         ////setting the fruit class
+//         newLi.className='fruit';
+
+ 
+
+     
+//       newLi.innerHTML=fruitToAdd.value  +'<button class="delete-btn">x</button>';
+//       // Adding Li as the tost element of unordered list
+//         fruits.appendChild(newLi);
+// })
+
+
+// //creating input feild for description
 // const inputElement = document.createElement('input');
 
 // // Set attributes and properties for the input element
 // inputElement.type = 'text';
-// inputElement.id = 'new-input';
+// inputElement.id = 'description';
 // inputElement.placeholder = 'Enter your text';
 // inputElement.style.fontStyle='italic';
 // // Find the parent element where you want to insert the input element
@@ -20,6 +49,7 @@
 
 
 
+// //filter fruit name from video
 // const filter =document.getElementById("filter");
 // filter.addEventListener("keyup",function(event){
 //     const textEntered=event.target.value.toLowerCase();
@@ -35,109 +65,4 @@
 //     }
 // });
 
-
-
-
-//Bard code
-
-const fruitsList = document.querySelector('.fruits');
-const filterInput = document.getElementById('filter');
-const fruitToAddInput = document.getElementById('fruit-to-add');
-const fruitDescriptionInput = document.createElement('input'); // Create new input for description
-
-// Set attributes for the new description input
-fruitDescriptionInput.id = 'description';
-fruitDescriptionInput.type = 'text';
-fruitDescriptionInput.placeholder = 'Enter fruit description';
-
-// Insert the new input before the submit button
-const submitButton = document.querySelector('button');
-submitButton.parentNode.insertBefore(fruitDescriptionInput, submitButton);
-
-// Function to show fruit description
-function showFruitDescription(fruit, description) {
-  const li = document.createElement('li');
-  li.classList.add('fruit');
-
-  const nameSpan = document.createElement('span');
-  nameSpan.textContent = fruit;
-  li.appendChild(nameSpan);
-
-  const descriptionParagraph = document.createElement('p');
-  descriptionParagraph.style.fontStyle = 'italic';
-  descriptionParagraph.textContent = description;
-  li.appendChild(descriptionParagraph);
-
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-btn');
-  deleteButton.textContent = 'x';
-  deleteButton.addEventListener('click', () => {
-    fruitsList.removeChild(li);
-  });
-  li.appendChild(deleteButton);
-
-  fruitsList.appendChild(li);
-}
-
-// Add fruit to list with description
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const fruit = fruitToAddInput.value;
-  const description = fruitDescriptionInput.value;
-
-  if (fruit && description) {
-    showFruitDescription(fruit, description);
-    fruitToAddInput.value = '';
-    fruitDescriptionInput.value = '';
-  } else {
-    alert('Please enter both fruit name and description');
-  }
-});
-
-// Filter fruits based on name or description
-filterInput.addEventListener('keyup', () => {
-  const filterText = filterInput.value.toLowerCase();
-  const fruits = fruitsList.querySelectorAll('.fruit');
-
-  fruits.forEach((fruit) => {
-    const name = fruit.querySelector('span').textContent.toLowerCase();
-    const description = fruit.querySelector('p').textContent.toLowerCase();
-    const isVisible = name.includes(filterText) || description.includes(filterText);
-    fruit.style.display = isVisible ? 'block' : 'none';
-  });
-});
-
-
-
-
-const filter =document.getElementById("filter");
-filter.addEventListener("keyup",function(event){
-    const textEntered=event.target.value.toLowerCase();
-    const fruitsItems=document.getElementsByClassName("fruit");
-    for(let i=0;i<fruitsItems.length;i++){
-        const currentFruitText=fruitsItems[i].firstChild.textContent.toLowerCase();
-if(currentFruitText.indexOf(textEntered)===-1){
-    fruitsItems[i].style.display="none";
-}
-else{
-    fruitsItems[i].style.display="flex";
-}
-    }
-});
-
-
-const filterdiscription = document.getElementById("description");
-filter.addEventListener("keyup",function(event){
-    const textEntered=event.target.value.toLowerCase();
-    const fruitsItems=document.getElementsByClassName("fruit");
-    for(let i=0;i<fruitsItems.length;i++){
-        const currentFruitText=fruitsItems[i].firstChild.textContent.toLowerCase();
-if(currentFruitText.indexOf(textEntered)===-1){
-    fruitsItems[i].style.display="none";
-}
-else{
-    fruitsItems[i].style.display="flex";
-}
-    }
-});
-localStorage.setItem()
+//function to filter description
